bf7a2676d509f4c60b5aa74b66842e9823560b2c
Borschik <47531847+Borschik1@users.noreply.github.com>
2022-02-27 12:13:36 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student{
+    int age;
+    string name;
+    string drink;
+};
+
+bool prime(int num) {
+    for (int i = 2; i < pow(num, 0.5) + 2; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    Student mas[3];
+    mas[0] = { 19, "Gordon", "Coffe" };
+    mas[1] = { 18, "Dante", "Beer" };
+    mas[2] = { 23, "Lara", "Tea" };
+
+    int maxi = 0;
+    for (int i = 0; i < 3; ++i) {
+        if (prime(mas[i].age)) {
+            cout << mas[i].name << " " << mas[i].drink << endl;
+        }
+    }
+    cout << maxi;
+    return 0;
+}
